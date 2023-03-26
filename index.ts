7cf991b0c1557f12035c32de8e5dde23cb6f0678
Kyle Saburao <ksaburao@sfu.ca>
2023-03-25 18:43:30 -0700

    add user data model

--- index.ts
+++ index.ts
@@ -2,4 +2,5 @@ export { generateLoginToken } from "./src/generateLoginToken";
 export { LoginParameters } from "./src/LoginParameters";
 export { LoginTokenizedRequest } from "./src/LoginTokenizedRequest";
 export { LoginTokenParameters } from "./src/LoginTokenParameters";
+export { User } from "./src/User";
 export { validateLoginToken } from "./src/verifyLoginToken";
